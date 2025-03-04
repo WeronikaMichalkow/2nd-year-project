88aa96c11d12812c9e32ba8a4daad9ca7c3391c5
WeronikaMichalkow <X00216232@mytudublin.ie>
2025-03-04 13:09:56 +0000

    Revert "Weronika"

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from store.views import mens_view
-from .views import homepage
-
-urlpatterns = [
-    path('mens/', mens_view, name='menspage'),
-    path('', homepage, name='homepage'),
-]
--- search/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import search_results
-
-urlpatterns = [
-    path('results/', search_results, name='search'),
-]
