0c067020af2cb6972ea2ff4ce85b26d58e1699dc
Weronika Michalkow <X00216232@mytudublin.ie>
2025-03-18 22:41:04 +0000

    Weronika

--- questions/models.py
+++ questions/models.py
@@ -1,16 +1,13 @@
 from django.db import models
-
-# Create your models here.
-
-from django.contrib.auth.models import User
+from django.conf import settings  # Import settings to use AUTH_USER_MODEL
 from store.models import Customer
 
 class Question(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)  # User who asked the question
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, default=1)  # Link to the Customer model
     text = models.TextField()
     answer = models.TextField(blank=True, null=True)  
     created = models.DateTimeField(auto_now_add=True)
     answered = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):  
-        return "Question by " + str(self.customer)
+    def __str__(self):
+        return str(self.user)  # Return the username or customer name if needed
--- questions/views.py
+++ questions/views.py
@@ -2,31 +2,42 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.timezone import now
 from .models import Question
 from .forms import form_for_question, form_for_answer
+from store.models import Customer  
 
 def askQ(request):
     if request.method == "POST":
-        form = QuestionForm(request.POST)
+        form = form_for_question(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
-            question.customer = request.user.customer  # Assuming every user has a related customer
+            question.user = request.user  # Assuming the user is logged in
             question.save()
-            return redirect('list')  # Redirect to the question list or another page
+            return redirect('questions:list')  # Correct way to redirect with app_name namespace
     else:
-        form = orm_for_question()
+        form = form_for_question()
+
     return render(request, 'ask_q.html', {'form': form})
+
 
 def listQ(request):
     questions = Question.objects.all()
-    return render(request, 'list.html', {'questions': questions})
+    return render(request, 'list.html', {'questions': questions})  # ✅ Rendering list.html
+
+
 
 def answerQ(request, question_id):
     question = get_object_or_404(Question, id=question_id)
+
+    # Check if the user is an admin (staff user)
+    if not request.user.is_staff:
+        return redirect('questions:list')  # Redirect non-admin users to the list of questions
+    
     if request.method == "POST":
         form = form_for_answer(request.POST, instance=question)
         if form.is_valid():
-            question.answered = now()
+            question.answered = now()  # Set the answer time
             form.save()
-            return redirect('list')
+            return redirect('questions:list')
     else:
         form = form_for_answer(instance=question)
+
     return render(request, 'answer_q.html', {'form': form, 'question': question})
