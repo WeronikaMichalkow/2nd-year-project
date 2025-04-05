a11b4892a1ab9309b6b0938677b258b4f3d575a6
sinead bankole <x00211681@mytudublin.ie>
2025-04-05 17:46:31 +0100

    sinead

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ReviewsView, SubmitReviewView
+from .views import ReviewsView, SubmitReviewView, delete_review
 
 urlpatterns = [
     path('', ReviewsView.as_view(), name='reviews'),  
     path('submit/', SubmitReviewView.as_view(), name='submit_review'),
+    path('delete/<int:review_id>/', delete_review, name='delete_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -5,6 +5,8 @@ from .forms import ReviewForm
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from store.models import Product
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 class ReviewsView(View):
     def get(self, request):
@@ -18,12 +20,12 @@ class ReviewsView(View):
             if form.is_valid():
                 review = form.save(commit=False)
                 review.user = request.user
-               
                 review.product = form.cleaned_data.get('product')
                 review.save()
                 return redirect('reviews')
         return redirect('cos_accounts:signin')
-        
+
+
 @method_decorator(csrf_exempt, name='dispatch')
 class SubmitReviewView(View):
     def post(self, request):
@@ -34,16 +36,26 @@ class SubmitReviewView(View):
         rating = request.POST.get('rating')
         product_id = request.POST.get('product')
 
-        
         product = get_object_or_404(Product, id=product_id)
 
-     
         if review_text and rating:
             Review.objects.create(
                 user=request.user,
-                product=product, 
+                product=product,
                 review_text=review_text,
                 rating=rating
             )
 
         return redirect(request.META.get('HTTP_REFERER', '/'))
+
+
+
+@login_required
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    if review.user != request.user:
+        return HttpResponseForbidden("You can't delete someone else's review.")
+
+    review.delete()
+    return redirect('reviews')
